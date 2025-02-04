e42c2d7e12160127870954ccf4ecae209c4ce6b9
Lina Villalba <linavilla1499@gmail.com>
2025-02-04 11:02:35 -0500

    Add new section

--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -10,6 +10,7 @@ export default function Topbar() {
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+  
 
   return (
     <header className="bg-black text-white w-full p-4 drop-shadow-2xl absolute z-1 ">
@@ -25,6 +26,9 @@ export default function Topbar() {
         >
           <a href="#about" className="hover:text-orange-50">
             About{" "}
+          </a>
+          <a href="#skills" className="hover:text-orange-50">
+            Skills
           </a>
           <a href="#projects" className="hover:text-orange-50">
             Projects
