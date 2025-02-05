97977a9a322587bc95b54e9bdb070fe3f64e4049
Lina Villalba <linavilla1499@gmail.com>
2025-02-05 12:22:03 -0500

    Refactor code

--- src/components/separator/separator.tsx
+++ src/components/separator/separator.tsx
@@ -1,6 +1,6 @@
 export default function Separator() {
   return (
-    <div className="flex items-center mb-10 w-full hidden sm:flex">
+    <div className="flex items-center mb-10 w-full hidden sm:flex ">
       <div className="flex-grow h-0.5 border-t-1  border-orange-50 opacity-15"></div>
       <div className="  w-1 h-1 rounded-full bg-orange-50  mx-4 opacity-15"></div>
       <div className="flex-grow h-0.5 border-t-1  border-orange-50 opacity-15"></div>
