4ab163b361ca0de2a27101f585795ccabc16f0ea
Lina Villalba <linavilla1499@gmail.com>
2025-02-05 12:20:36 -0500

    Add animate section intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect } from "react";
+
 import Topbar from "@/components/topbar/topbar";
 import Intro from "@/components/intro/intro";
 import Projects from "@/components/projects/projects";
@@ -8,36 +8,6 @@ import About from "@/components/about/about";
 import Skills from "@/components/skills/skills";
 
 export default function Home() {
-  const lightColor = "rgba(112, 104, 104, 0.5)";
-  const gradientSize = 150;
-  useEffect(() => {
-    const playpen = document.getElementById("root");
-    if (!playpen) return;
-
-    const originalBGplaypen = getComputedStyle(playpen).backgroundColor;
-
-    // Función para manejar el evento de movimiento del mouse en el playpen
-    const handleMouseMovePlaypen = (e: MouseEvent) => {
-      const x = e.pageX - playpen.offsetLeft;
-      const y = e.pageY - playpen.offsetTop;
-      const xy = `${x} ${y}`;
-
-      const bgWebKit = `-webkit-gradient(radial, ${xy}, 0, ${xy}, ${gradientSize}, from(${lightColor}), to(rgba(255,255,255,0.0))), ${originalBGplaypen}`;
-
-      const bgMoz = `-moz-radial-gradient(${x}px ${y}px 45deg, circle, ${lightColor} 0%, ${originalBGplaypen} ${gradientSize}px)`;
-
-      playpen.style.background = bgWebKit;
-      playpen.style.background = bgMoz;
-    };
-
-    const handleMouseLeavePlaypen = () => {
-      playpen.style.background = originalBGplaypen;
-    };
-
-    playpen.addEventListener("mousemove", handleMouseMovePlaypen);
-    playpen.addEventListener("mouseleave", handleMouseLeavePlaypen);
-  }, []);
-
   return (
     <main
       id="root"
