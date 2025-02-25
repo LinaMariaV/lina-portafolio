4ed6049258d749f94b67c35ab00caea8b3d85459
Lina Villalba <linavilla1499@gmail.com>
2025-02-25 16:53:39 -0500

    Add animate

--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -1,8 +1,8 @@
-"use client";
-
 import { useState } from "react";
 import Image from "next/image";
 import logolv from "../../../public/lv-logo-violet.png";
+import "./topbar.css";
+
 
 interface NavLink {
   href: string;
@@ -30,33 +30,39 @@ export default function Topbar() {
   };
 
   return (
-    <header className="bg-black text-white w-full p-4 drop-shadow-2xl z-1 sticky top-0">
+    <header className="bg-black text-white w-full p-4 drop-shadow-2xl sticky top-0 z-10">
       <nav className="flex items-center justify-between">
         <div className="mx-3">
           <Image width={45} height={45} src={logolv} alt="Lina Villalba" />
         </div>
 
-        <div
-          className={`${
-            menuOpen ? "block" : "hidden"
-          } sm:flex space-x-8 items-center transition-all duration-300 ease-in-out`}
-        >
+        <div className="hidden sm:flex space-x-8 items-center">
+          
           {navLinks.map(({ href, label }) => (
             <a
               key={href}
-              className="hover:text-orange-50 cursor-pointer"
+              className="nav-item cursor-pointer " 
               onClick={() => ScrollTo(href)}
             >
               {label}
             </a>
           ))}
 
-          <button className="text-indigo-500 font-bold py-2 px-4 rounded">
+          <a
+            className="nav-item text-indigo-500 font-bold py-2 px-4 rounded cursor-pointer"
+            href="/LinaVillalbaCV.pdf"
+            target="_blank"
+            rel="noreferrer"
+          >
             View CV
-          </button>
+          </a>
         </div>
 
-        <button className="sm:hidden text-orange-50" onClick={ToggleMenu}>
+        <button
+          className="sm:hidden text-orange-50"
+          onClick={ToggleMenu}
+          aria-label="Toggle menu"
+        >
           <svg
             className="w-6 h-6"
             fill="none"
@@ -73,6 +79,34 @@ export default function Topbar() {
           </svg>
         </button>
       </nav>
+
+      {/* Menú móvil */}
+      <div
+        className={`${
+          menuOpen ? "block" : "hidden"
+        } sm:hidden w-full bg-black text-white p-4 space-y-4 text-center transition-all ease-in-out`}
+      >
+        {navLinks.map(({ href, label }) => (
+          <a
+            key={href}
+            className="nav-item block  cursor-pointer"
+            onClick={() => {
+              ScrollTo(href);
+              setMenuOpen(false);
+            }}
+          >
+            {label}
+          </a>
+        ))}
+        <a
+          className="nav-item block text-indigo-500 font-bold py-2 px-4 rounded"
+          href="/LinaVillalbaCV.pdf"
+          target="_blank"
+          rel="noreferrer"
+        >
+          View CV
+        </a>
+      </div>
     </header>
   );
 }
